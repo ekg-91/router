96aa308510c894cb5c27ec1e560df5305df64696
EKG <elliotgallagher91@gmail.com>
2018-07-25 11:02:58 -0400

    router-final-commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import Contact from './components/Contact';
+import About from './components/About';
+import Error from './components/Error';
+import Navigation from './components/Navigation';
+
+const NewRoute = () => {
+  return(
+    <div>
+      <p>New Route</p>
+    </div>
+  );
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-
-      </div>
+      <BrowserRouter>
+        <div>
+          <Navigation />
+          <Switch>
+            <Route path='/' component={Home} exact />
+            <Route path='/about' component={About} />
+            <Route path='/contact' component={Contact} />
+            <Route component={Error} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
